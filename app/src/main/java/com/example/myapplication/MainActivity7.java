4bdd660d6f1408d6a87e4081391b3f0b80d2b53c
bysghjx <74447058+bysghjx@users.noreply.github.com>
2022-06-24 17:51:59 +0800

    Update MainActivity7.java

--- app/src/main/java/com/example/myapplication/MainActivity7.java
+++ app/src/main/java/com/example/myapplication/MainActivity7.java
@@ -89,44 +89,6 @@ public class MainActivity7 extends AppCompatActivity {
             if (TextUtils.isEmpty(name)) {
                 Toast.makeText(MainActivity7.this, "name不能为空", Toast.LENGTH_SHORT).show();
             } else {
-                /*
-                FutureTask<Void> task = new FutureTask<>(new MojangHttps(MainActivity7.this, input_name, result -> {
-                    if(sp.getString("uuid",null) != null){
-                        Thread thread1 = new HypixelHttps(uuid,api_key);
-                        Log.e("join hyp","join hyp");
-                        thread1.start();
-
-                        GetPlayerName gpn =  new GetPlayerName(MainActivity7.this);
-
-                        String Pdata = gpn.getData();
-
-                        editor.putString("Pdata",Pdata);
-                        editor.commit();
-                    }
-                }));
-                new Thread(task).start();
-                */
-
-//                    String uuid = MojangUtils.getUUIDByName(input_name);
-//                    if (uuid == null) {
-//                        Toast.makeText(MainActivity7.this, "找不到对应uuid!请检查用户名是否输入正确" , Toast.LENGTH_SHORT).show();
-//                        return;
-//                    }
-//                    HypixelPlayerInfo pi = new HypixelPlayerInfo();
-//                    int code = HypixelUtils.getPlayer(uuid, pi);
-//                    if (code == -1) {
-//                        Toast.makeText(MainActivity7.this, "查询失败", Toast.LENGTH_SHORT).show();
-//                        return;
-//                    }
-//
-//                    if (code == 200) {
-//                        Intent intent = new Intent(MainActivity7.this,MainActivity.class);
-//                        intent.putExtra("data",pi.toString());
-//                        startActivity(intent);
-//                        return;
-//                    }
-//
-//                    Toast.makeText(MainActivity7.this, "Http: " + code, Toast.LENGTH_SHORT).show();
 
                 FutureTask<HypixelPlayerInfo> task = new FutureTask<>(new HypixelPlayerQuery(input_name, s ->
                         Toast.makeText(MainActivity7.this, s, Toast.LENGTH_SHORT).show()
