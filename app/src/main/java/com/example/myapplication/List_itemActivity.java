71a771522554f4724e06e64693377fa52ee2c1f0
bysghjx <bysg1044@gmail.com>
2022-11-12 16:26:55 +0800

    优化Bazaar查询并存入数据库的速度,加入Bazaar列表显示物品

---
+++ app/src/main/java/com/example/myapplication/List_itemActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class List_itemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+    }
+}
